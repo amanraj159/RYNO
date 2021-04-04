641570da75206add04fcafb4c3a87a12d3e80b63
RYNO-X <73129419+RYNO-X@users.noreply.github.com>
2021-04-04 14:46:52 +0530

    Update gban_mute_db.py

--- RYNO/functions/gban_mute_db.py
+++ RYNO/functions/gban_mute_db.py
@@ -5,7 +5,7 @@
 # PLease read the GNU Affero General Public License in
 # <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
 
-from pyUltroid import udB
+from RYNO import udB
 
 
 def str_to_list(text):
